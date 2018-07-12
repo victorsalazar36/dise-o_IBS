62501ac4193d824a8fb3e09c5c980868939681ce
Angel Rada <angelgrv20@gmail.com>
2018-07-11 21:46:54 -0400

    fixed navbar

--- login.php
+++ login.php
@@ -4,40 +4,33 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>IBS</title>
-	<link rel="stylesheet" href="css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-	 <link href="css/floating-labels.css" rel="stylesheet">
-
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/floating-labels.css">
 </head>
 <body>
 	
-	 <form class="form-signin">
-      <div class="text-center mb-4">
-        <img class="mb-4" src="img/bootstrap-solid.svg" alt="" width="72" height="72">
-        <h1 class="h3 mb-3 font-weight-normal">Invoice and Budged Simple</h1>
-        <p>Gestione y controle sus facturas y presupuestos de manera simple y eficiente. </p>
-      </div>
+	<form class="form-signin">
+        <div class="text-center mb-4">
+            <img class="mb-4" src="img/bootstrap-solid.svg" alt="" width="72" height="72">
+            <h1 class="h3 mb-3 font-weight-normal">Invoice and Budged Simple</h1>
+            <p>Gestione y controle sus facturas y presupuestos de manera simple y eficiente. </p>
+        </div>
 
-      <div class="form-label-group">
-        <input type="text" id="inputUsuario" class="form-control" placeholder="User address" required autofocus>
-        <label for="inputUsuario">Usuario</label>
-      </div>
+        <div class="form-label-group">
+            <input type="text" id="inputUsuario" class="form-control" placeholder="User address" required autofocus>
+            <label for="inputUsuario">Usuario</label>
+        </div>
 
-      <div class="form-label-group">
-        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-        <label for="inputPassword">Contraseña</label>
-      </div>
+        <div class="form-label-group">
+            <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+            <label for="inputPassword">Contraseña</label>
+        </div>
 
-      <button class="btn btn-lg btn-primary btn-block" type="submit">Iniciar Sesion</button>
-      <p class="mt-5 mb-3 text-muted text-center">&copy; 2018</p>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Iniciar Sesion</button>
+        <p class="mt-5 mb-3 text-muted text-center">&copy; 2018</p>
     </form>
 
-
-
-
-
-
-	<script src="js/jquery-3.3.1.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-
-  <script src="js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+	<script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
