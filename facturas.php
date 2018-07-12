5ca6990288274fcf87c45843fdb43d15645224b5
victorsalazar36 <victor36salazar@gmail.com>
2018-07-11 20:50:32 -0400

    Diseño basico de IBS

---
+++ facturas.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Facturas</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+	<script src="js/all.js"></script>
+	<link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+<header>	
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #428bca">
+<div class="container-fluid">
+  <a class="navbar-brand" href="#">IBS</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="nav navbar-nav">
+    	<li class="nav-item dropdown active">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-file-invoice"></i>
+          Facturas
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Ventas</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Compras</a>
+        </div>
+       	</li>
+
+	    <li class="nav-item">
+    	    <a class="nav-link" href="#">Presupuestos</a>
+      	</li>
+
+      	<li class="nav-item">
+        <a class="nav-link" href="#"><i class="fas fa-barcode"></i>Productos</a>
+        </li>
+
+      	<li class="nav-item">
+        <a class="nav-link" href="#">Clientes</a>
+      	</li>
+
+      	<li class="nav-item">
+        <a class="nav-link" href="#">Proveedores</a>
+      	</li>
+
+      	<li class="nav-item">
+        <a class="nav-link" href="#">Usuarios</a>
+      	</li>
+
+      	<li class="nav-item">
+        <a class="nav-link" href="#">Configuración</a>
+      	</li>
+      	
+      	<ul>
+      	<li class="nav navbar-nav navbar-right"><a class="nav-link" href="#"><i class="fas fa-power-off"></i>Salir</a></li>
+		</ul>
+
+    </ul>
+  </div>
+</div>
+</nav>
+</header>
+
+<script src="js/jquery-3.3.1.min.js"></script>
+
+<script src="js/bootstrap.min.js"></script>
+
+</body>
+</html>
